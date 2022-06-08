31e79faff606665e1d875b068d01d8de0b15a87f
Julian <julian.cubillos@jikkosoft.com>
2022-06-08 17:17:08 -0500

    change variables into functions

--- README.md
+++ README.md
@@ -2,25 +2,26 @@
 
 Algunos talleres correspondientes al curso practico de Javascript
 
-## Breve explicación Taller 1 Calculadora perimetros y areas figuras geometricas
-1 - Definición de formulas
-2 - Implementar formulas en Javascript
-3 - Creación de funciones
-4 - Integración js con Html
+## Breve explicación Taller 1 
+## Calculadora perimetros y areas figuras geometricas
+###1 - Definición de formulas
+###2 - Implementar formulas en Javascript
+###3 - Creación de funciones
+###4 - Integración js con Html
 
 - Formulas
-    Perímetro: Suma de los lados de la figura.
-    Área: Superficie interna de la figura.
+    - Perímetro: Suma de los lados de la figura.
+    - Área: Superficie interna de la figura.
 
-    Cuadrado
-        Perímetro: l*4, donde ‘l’ es lado
-        Área: l^2 ó l*l
+    - Cuadrado
+        - Perímetro: l*4, donde ‘l’ es lado
+        - Área: l^2 ó l*l
 
-    Triángulo
-        Perímetro: l+l+l
-        Área: (b*a)/2, donde ‘b’ es base y ‘a’ es altura
+    - Triángulo
+        - Perímetro: l+l+l
+        - Área: (b*a)/2, donde ‘b’ es base y ‘a’ es altura
 
-    Circunferencia
-        Perímetro: Diámetro * PI, donde PI = 3.1416
-        Área: PI * Radio^2
+    - Circunferencia
+        - Perímetro: Diámetro * PI, donde PI = 3.1416
+        - Área: PI * Radio^2
 
--- taller1/geometric_figures.js
+++ taller1/geometric_figures.js
@@ -1,56 +1,61 @@
 //Codigo del cuadrado
 console.group("Cuadrado");
-const squareSide = 5;
-console.log("Los lados del cuadrado miden: " + squareSide + " cm");
+// const squareSide = 5;
+// console.log("Los lados del cuadrado miden: " + squareSide + " cm");
 
-const squarePerimeter = squareSide * 4;
-console.log("el perimetro del cuadrado mide: " + squarePerimeter + " cm");
+const squarePerimeter = (squareSide) => squareSide * 4;
+// console.log("el perimetro del cuadrado mide: " + squarePerimeter + " cm");
 
-const squareArea = squareSide * squareSide;
-console.log("el area del cuadrado mide: " + squareArea + " cm^2");
+const squareArea = (squareSide) => squareSide * squareSide;
+// console.log("el area del cuadrado mide: " + squareArea + " cm^2");
 console.groupEnd();
 
 //Codigo del triángulo
 console.group("Triangulo");
-const triangleSide1 = 6;
-const triangleSide2 = 6;
-const triangleBase = 4;
-const triangleHigh = 5.5;
+// const triangleSide1 = 6;
+// const triangleSide2 = 6;
+// const triangleBase = 4;
+// const triangleHigh = 5.5;
 
-console.log(
-  "Los lados del triangulo miden: " +
-    triangleSide1 +
-    " cm, " +
-    triangleSide2 +
-    " cm, y " +
-    triangleBase +
-    " cm"
-);
+// console.log(
+//   "Los lados del triangulo miden: " +
+//     triangleSide1 +
+//     " cm, " +
+//     triangleSide2 +
+//     " cm, y " +
+//     triangleBase +
+//     " cm"
+// );
 
-console.log("La altura del triangulo es: " + triangleHigh + " cm");
+// console.log("La altura del triangulo es: " + triangleHigh + " cm");
 
-const trianglePerimeter = triangleSide1 + triangleSide2 + triangleBase;
-console.log("el perimetro del cuadrado mide: " + trianglePerimeter + " cm");
+const trianglePerimeter = (triangleSide1, triangleSide2, triangleBase) =>
+  triangleSide1 + triangleSide2 + triangleBase;
+// console.log("el perimetro del cuadrado mide: " + trianglePerimeter + " cm");
 
-const triangleArea = (triangleBase * triangleHigh) / 2;
-console.log("el area del cuadrado mide: " + triangleArea + " cm^2");
+const triangleArea = (triangleBase, triangleHigh) =>
+  (triangleBase * triangleHigh) / 2;
+// console.log("el area del cuadrado mide: " + triangleArea + " cm^2");
 
 console.groupEnd();
 
 //Codigo del circulo
-console.group("Circulo");
-const circleRadio = 4;
-console.log("El radio del circulo mide: " + circleRadio + " cm");
+// console.group("Circulo");
+// const circleRadio = 4;
+// console.log("El radio del circulo mide: " + circleRadio + " cm");
 
-const circleDiameter = circleRadio * 2;
-console.log("el diametro del circulo mide: " + circleDiameter + " cm");
+const circleDiameter = (circleRadio) => circleRadio * 2;
+// console.log("el diametro del circulo mide: " + circleDiameter + " cm");
 
 const pi = Math.PI;
 console.log("PI mide: " + pi);
 
-const circlePerimeter = pi * circleDiameter;
-console.log("el perimetro del circulo mide: " + circlePerimeter + " cm");
+const circlePerimeter = (circleRadio) => {
+  const diameter = circleDiameter(circleRadio);
+  return pi * diameter;
+};
+// console.log("el perimetro del circulo mide: " + circlePerimeter + " cm");
 
-const circleArea = pi * (circleRadio * circleRadio);
-console.log("el area del circulo mide: " + circleArea + " cm^2");
+const circleArea = (circleRadio, pi) => pi * (circleRadio * circleRadio);
+// console.log("el area del circulo mide: " + circleArea + " cm^2");
 console.groupEnd();
