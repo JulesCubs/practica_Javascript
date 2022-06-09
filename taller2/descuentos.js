4020ab63983943cb8334203497636f12f48ba1c1
Julian <julian.cubillos@jikkosoft.com>
2022-06-09 15:01:21 -0500

    add function in taller2

--- taller2/descuentos.js
+++ taller2/descuentos.js
@@ -1,8 +1,12 @@
 const price = 120;
 const discount = 18;
 
-const percentageWithoutDiscount = 100 - discount;
-const priceWithDiscount = (price * percentageWithoutDiscount) / 100;
+const calculatePriceWithDiscount = (price, discount) => {
+  const percentageWithoutDiscount = 100 - discount;
+  const priceWithDiscount = (price * percentageWithoutDiscount) / 100;
+
+  return priceWithDiscount;
+};
 
 console.log({
   price,
